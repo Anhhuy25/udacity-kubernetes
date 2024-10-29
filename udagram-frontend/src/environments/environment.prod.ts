3d15a4462da2ba1b3bb2291d4816717f4af1cbee
huyvda <vuducanhhuy@gmail.com>
2024-10-29 22:45:42 +0700

    replace url to src frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://aac1250fb96f14cd3a63784bb730aec3-1980506555.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
